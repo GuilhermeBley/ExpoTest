5db144711cd086fae64213aee7ee87fcb9e43f9b
Guilherme Bley <guilhermecbley@gmail.com>
2024-05-19 20:12:24 -0300

    add form inputs

--- BlHealth/components/WeigthForm/index.js
+++ BlHealth/components/WeigthForm/index.js
@@ -1,20 +1,49 @@
+import { useState } from 'react';
 import { Text, TextInput, View, Button } from 'react-native';
 
 export function Component() {
+
+    const defaultMessage = "Fill the form to calculate your BMI"
+
+    const [weigth, setWeigth] = useState(null);
+    const [height, setHeight] = useState(null);
+    const [message, setMessage] = useState(defaultMessage);
+
+    function calculateImc()
+    {
+        if (weigth === null) {
+            setMessage(defaultMessage);
+            return
+        }
+        if (height === null) {
+            setMessage(defaultMessage);
+            return
+        }
+
+        let bmi = (weigth/(height*height)).toFixed(2)
+        setMessage("Your BMI is " + bmi + ".")
+    }
+
     return (
         <View>
             <Text>Heigth</Text>
             <TextInput 
                 placeholder='Ex. 1.2'
+                value={height}
+                onChange={setHeight}
                 keyboardType='numeric'/>
 
 
             <Text>Weigth</Text>
             <TextInput 
+                value={weigth}
+                onChange={setWeigth}
                 placeholder='Ex. 70'
                 keyboardType='numeric'/>
 
-            <Button title='Calculate'/>
+            <Button onPress={calculateImc} title='Calculate' />
+            
+            <Text>{message}</Text>
         </View>
     );
 }
